bfb6c0928bc2fb3a117d2dc6e100f379b93f4ab3
Long Nguyen <18nguyenl@gmail.com>
2025-07-26 00:43:26 -0500

    feat: add xdg config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,3 +1,11 @@
+# Set XDG configuration defaults
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
+set -gx XDG_CONFIG_DIRS /etc/xdg
+set -gx XDG_CACHE_HOME $HOME/.cache
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
