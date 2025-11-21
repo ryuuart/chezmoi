2bf4c881b11cad502a149f697ce93f9c9eff5eb8
Long Nguyen <me@long-nguyen.dev>
2025-11-21 14:59:07 -0600

    chore: update configs

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,17 +6,36 @@ set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
 set -gx XDG_CONFIG_DIRS /etc/xdg
 set -gx XDG_CACHE_HOME $HOME/.cache
 
+# macos config
+if test (uname) = Darwin
+    # set locale
+    set -gx LANG en_US.UTF-8
+    set -gx LC_ALL en_US.UTF-8
+
+    # bit
+    if not string match -q -- /Users/long/bin $PATH
+        set -gx PATH $PATH /Users/long/bin
+    end
+    # bit end
+
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
+# Configure interactive shell settings
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
     # mise - dev environment and tools
     mise=(which mise) $mise activate fish | source
 
+    # chezmoi
+    chezmoi completion fish | source
+
     # zoxide - improved & smarter cd
     zoxide init fish | source
 
-    # set default editor to neovim
-    set -gx EDITOR nvim
+    # set default editor to zed
+    set -gx EDITOR zed
 
     # add completion scripts
     gh completion -s fish | source
